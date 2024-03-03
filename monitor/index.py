e0071ad1b9cdd2e64f54f8c4153274dbcb215ab7
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-03 12:41:12 +0300

    a

--- monitor/index.py
+++ monitor/index.py
@@ -2,18 +2,13 @@ from gpiozero import CPUTemperature
 import serial
 import serial.tools.list_ports
 
-port = "COM1"
-baudrate = 9600
-ser = serial.Serial(port, baudrate=baudrate)
+# port = "/dev/ttyUSB0"
 
 ports = serial.tools.list_ports.comports()
 
 for port in ports:
-    print(port.device)
-
-ser.write(b"0,192.168.31.201")
-
-cpu = CPUTemperature()
-ser.write(b"1,{cpu.temperature}")
-
-ser.close()
+    ser = serial.Serial(port, baudrate=9600)
+    ser.write(b"0,192.168.31.201")
+    cpu = CPUTemperature()
+    ser.write(b"1,{cpu.temperature}")
+    ser.close()
