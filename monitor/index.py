43e937a44ea0e0c07b32c0f697ca80bfbe539241
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-03 12:59:36 +0300

    a

--- monitor/index.py
+++ monitor/index.py
@@ -18,7 +18,7 @@ try:
         if line and line == "Hello":
             ser.write(b"0,192.168.31.201\x0D")
             cpu = CPUTemperature()
-            ser.write(b"1,{}\x0D".format(cpu.temperature))
+            ser.write(bytes("1,{}\x0D".format(cpu.temperature)))
 
 except ValueError as ve:
     print("Error:", str(ve))
