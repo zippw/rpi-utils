20b96c65c7ad8460d3bd190fa1c9bb7272d14262
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-03 12:43:50 +0300

    a

--- monitor/index.py
+++ monitor/index.py
@@ -4,11 +4,25 @@ import serial.tools.list_ports
 
 # port = "/dev/ttyUSB0"
 
-ports = serial.tools.list_ports.comports()
+try:
+    ports = serial.tools.list_ports.comports()
+    port = next((p.device for p in ports), None)
+    if port is None:
+        raise ValueError("No COM port found.")
 
-for port in ports:
     ser = serial.Serial(port, baudrate=9600)
+
     ser.write(b"0,192.168.31.201")
     cpu = CPUTemperature()
     ser.write(b"1,{cpu.temperature}")
+
     ser.close()
+
+except ValueError as ve:
+    print("Error:", str(ve))
+
+except serial.SerialException as se:
+    print("Serial port error:", str(se))
+
+except Exception as e:
+    print("An error occurred:", str(e))
