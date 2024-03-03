744115c8f30c22a9a926a259a7968b4cd59117ce
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-03 12:32:14 +0300

    test serial

--- monitor/index.py
+++ monitor/index.py
@@ -1,4 +1,19 @@
 from gpiozero import CPUTemperature
+import serial
+import serial.tools.list_ports
+
+port = "COM1"
+baudrate = 9600
+ser = serial.Serial(port, baudrate=baudrate)
+
+ports = serial.tools.list_ports.comports()
+
+for port in ports:
+    print(port.device)
+
+ser.write(b"0,192.168.31.201")
 
 cpu = CPUTemperature()
-print(cpu.temperature)
+ser.write(b"1,{cpu.temperature}")
+
+ser.close()
