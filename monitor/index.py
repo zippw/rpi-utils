f2102e5ecd3e0fb4f0ff63da8d2f9a2a9781ff51
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-05 16:59:37 +0300

    one sec

--- monitor/index.py
+++ monitor/index.py
@@ -3,6 +3,7 @@ import psutil
 import serial
 from datetime import datetime
 import serial.tools.list_ports
+import time
 
 try:
     ports = serial.tools.list_ports.comports()
@@ -12,18 +13,32 @@ try:
 
     ser = serial.Serial(port, baudrate=9600)
 
-    while True:
-        line = ser.readline().decode().strip()
+    prev_cpu_temp = None
+    prev_memory_available = None
+    prev_time = None
 
-        if line and line == "Hello":
-            # ser.write(b"0,192.168.31.201\x0D")
+    while True:
+        msg = ser.readline().decode().strip()
+
+        if msg and msg == "Hello":
             cpu = CPUTemperature()
-            ser.write(bytes("0,{}\x0D".format(cpu.temperature), encoding='utf-8'))
-            ser.write(bytes("1,{}\x0D".format(round(psutil.virtual_memory().available / (2 ** 30), 2)), encoding='utf-8'))
-            ser.write(bytes("2,{}\x0D".format(str(datetime.now().strftime("%H:%M"))), encoding='utf-8'))
+            current_cpu_temp = round(cpu.temperature, 2)
+            current_memory_available = round(psutil.virtual_memory().available / (2 ** 30), 2)
+            current_time = str(datetime.now().strftime("%H:%M"))
+
+            if (current_cpu_temp != prev_cpu_temp) or (current_memory_available != prev_memory_available) or (current_time != prev_time):
+                ser.write(bytes("0,{}\x0D".format(current_cpu_temp), encoding='utf-8'))
+                ser.write(bytes("1,{}\x0D".format(current_memory_available), encoding='utf-8'))
+                ser.write(bytes("2,{}\x0D".format(current_time), encoding='utf-8'))
+
+                prev_cpu_temp = current_cpu_temp
+                prev_memory_available = current_memory_available
+                prev_time = current_time
+
+        time.sleep(1)
 
 except ValueError as ve:
-    print("Error:", str(ve))
+    print("ValueError:", str(ve))
 
 except serial.SerialException as se:
     print("Serial port error:", str(se))
