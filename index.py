cd3f1d905d6a79aec5323ee40b054b35cd248d38
Mosunov Valeriy <zippwickness@gmail.com>
2024-03-14 17:23:11 +0300

    btn test

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
-import sacn
 from rpi_ws281x import Color, PixelStrip, ws
+import RPi.GPIO as GPIO
+import sacn
 import numpy
 import time
 import sys
 
 LED_COUNT = [18, 30, 7]
 LED_PIN = [12, 21, 10]
+
+BUTTON_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+lights_check = True
+
+while True:
+    input_state = GPIO.input(BUTTON_PIN)
+    if input_state == False:
+        lights_check = not lights_check
+        print('Button Pressed', lights_check)
+        time.sleep(0.2)
 
 strips = [
     PixelStrip(LED_COUNT[0], LED_PIN[0]),
