aba4ff9e2215b5afc0aba1f5785855ae123d1f71
Mosunov Valeriy <zippwickness@gmail.com>
2024-05-20 18:47:18 +0300

    test

--- index.py
+++ index.py
@@ -1,149 +1,108 @@
-from rpi_ws281x import Color, PixelStrip, ws
+from rpi_ws281x import Color, PixelStrip
 import RPi.GPIO as GPIO
 import sacn
-import numpy
 import time
-import sys
 
-# setup lights switch
+# Constants
 LIGHTS_SWITCH_BUTTON_PIN = 17
 LIGHTS_SWITCH_FADE_TIME = 0.3
+LED_COUNT = [18, 30, 7]
+LED_PIN = [10, 21, 18]
+DEFAULT_LIGHT = [2]
+STEP = 10
+COLORS = [
+    [255, 113, 206], [185, 103, 255], [1, 205, 254],
+    [5, 255, 161], [255, 251, 250], [255, 113, 206]
+]
 
+# GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LIGHTS_SWITCH_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# setup lights
-LED_COUNT = [18, 30, 7]
-LED_PIN = [10, 21, 18]
-DEFAULT_LIGHT = [2]  # indexes, ex. 2 -> LED_COUNT[2]
-
-strips = [
-    PixelStrip(LED_COUNT[0], LED_PIN[0]),
-    PixelStrip(LED_COUNT[1], LED_PIN[1]),
-    PixelStrip(LED_COUNT[2], LED_PIN[2]),
-]
-
-for i in range(len(strips)):
-    strips[i].begin()
+# Setup PixelStrips
+strips = [PixelStrip(LED_COUNT[i], LED_PIN[i]) for i in range(len(LED_COUNT))]
+for strip in strips:
+    strip.begin()
 
 # Gradient generator
-step = 10
+def generate_gradient(colors, step):
+    gradient = []
+    for i in range(len(colors)):
+        gradient.append(colors[i])
+        next_color = colors[(i + 1) % len(colors)]
+        for j in range(1, step):
+            gradient.append([
+                round(colors[i][k] + ((next_color[k] - colors[i][k]) / step * j))
+                for k in range(3)
+            ])
+    return gradient
 
+gradient = generate_gradient(COLORS, STEP)
 
-def stepped(c1, c2, i):
-    return round(c1 + ((c2 - c1) / step * i))
-
-
-colors = [
-    [255, 113, 206],
-    [185, 103, 255],
-    [1, 205, 254],
-    [5, 255, 161],
-    [255, 251, 250],
-    [255, 113, 206],
-]
-gradient = []
-for i in range(len(colors)):
-    gradient.append(colors[i])
-    n = (i + 1) % len(colors)
-    for j in range(1, step):
-        gradient.append(
-            [
-                stepped(colors[i][0], colors[n][0], j),
-                stepped(colors[i][1], colors[n][1], j),
-                stepped(colors[i][2], colors[n][2], j),
-            ]
-        )
-
-lenta = gradient[slice(0, 7)]
-
-
-def gen_gradient(gradient, c):
-    # str = "\x1B[0;0H\x1B[0J"
-    for i in range(len(lenta)):
-        r, g, b = gradient[(c + i) % len(gradient)]
-        # str += "\x1B[48;2;{};{};{}m  \x1B[0m".format(int(r), int(g), int(b))
-        for s in range(len(DEFAULT_LIGHT)):
-            strips[s].setPixelColor(i, Color(r, g, b))
-    # print(str)
-    c = (c + 1) % len(gradient)
-    for s in range(len(DEFAULT_LIGHT)):
-        strips[s].show()
-    return c
-
+def update_lights(gradient, index):
+    for i in range(len(DEFAULT_LIGHT)):
+        for j in range(LED_COUNT[DEFAULT_LIGHT[i]]):
+            r, g, b = gradient[(index + j) % len(gradient)]
+            strips[DEFAULT_LIGHT[i]].setPixelColor(j, Color(r, g, b))
+        strips[DEFAULT_LIGHT[i]].show()
+    return (index + 1) % len(gradient)
 
 def process_packet(packet):
-    for i in range(sum(LED_COUNT)):
-        try:
-            r = packet.dmxData[i * 3]
-            g = packet.dmxData[i * 3 + 1]
-            b = packet.dmxData[i * 3 + 2]
-
-            for strip_index in range(len(strips)):
-                if i < LED_COUNT[strip_index]:
-                    strips[strip_index].setPixelColor(i, Color(r, g, b))
-                    break
-                else:
-                    i -= LED_COUNT[strip_index]
-
-        except IndexError:
-            pass
-
-    for i in range(len(strips)):
-        strips[i].show()
-
+    pixel_index = 0
+    for strip_index, strip in enumerate(strips):
+        for _ in range(LED_COUNT[strip_index]):
+            if pixel_index * 3 + 2 < len(packet.dmxData):
+                r, g, b = packet.dmxData[pixel_index * 3: pixel_index * 3 + 3]
+                strip.setPixelColor(pixel_index, Color(r, g, b))
+            pixel_index += 1
+        strip.show()
 
 def ease_in_out_quint(x):
     return 16 * x**5 if x < 0.5 else 1 - ((-2 * x + 2) ** 5) / 2
 
+def fade_lights(on):
+    for i in range(21):
+        ease_value = ease_in_out_quint(i / 20)
+        brightness = 255 * ease_value if on else 255 - (255 * ease_value)
+        for strip in strips:
+            if strips.index(strip) not in DEFAULT_LIGHT:
+                strip.setBrightness(round(brightness))
+                strip.show()
+        time.sleep(LIGHTS_SWITCH_FADE_TIME / 20)
 
+# Main loop
 if __name__ == "__main__":
     try:
-        # setup local server
         receiver = sacn.sACNreceiver()
         receiver.start()
 
         @receiver.listen_on("universe", universe=1)
         def callback(packet):
-            if lights_check == True:
+            if lights_check:
                 process_packet(packet)
+
         @receiver.listen_on("availability")
-        def callback(universe, changed):
-            print(changed)
+        def availability_callback(universe, changed):
             global lights_check
-            if (changed == "available"):
+            if changed == "available":
                 lights_check = True
             else:
                 lights_check = False
+                print(f"Universe {universe} is now {changed}")
 
-        receiver.join_multicast(1)
-
-        # lights switch
         lights_check = True
-        c = 0  # default light counter
+        gradient_index = 0
+
         while True:
-            input_state = GPIO.input(LIGHTS_SWITCH_BUTTON_PIN)
-            if input_state == False:
+            if GPIO.input(LIGHTS_SWITCH_BUTTON_PIN) == False:
                 lights_check = not lights_check
-
-                for i in range(21):
-                    x = i / 20
-                    ease_value = ease_in_out_quint(x)
-                    brightness = (
-                        255 * ease_value if lights_check else 255 - (255 * ease_value)
-                    )
-
-                    for i in range(len(strips)):
-                        if i not in DEFAULT_LIGHT:
-                            strips[i].setBrightness(round(brightness))
-                            strips[i].show()
-
-                    time.sleep(LIGHTS_SWITCH_FADE_TIME / 20)
-
+                fade_lights(lights_check)
                 print("Button Pressed", lights_check)
-            if lights_check == False:
-                c = gen_gradient(gradient, c)
+
+            if not lights_check:
+                gradient_index = update_lights(gradient, gradient_index)
                 time.sleep(0.05)
+
     except KeyboardInterrupt:
         receiver.leave_multicast(1)
         receiver.stop()
