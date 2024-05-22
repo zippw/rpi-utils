7b15fa09e0d0d003ce5f811fc216df7895aedead
Mosunov Valeriy <zippwickness@gmail.com>
2024-05-22 23:20:12 +0300

    test

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from modules.LightController import LightController
 from modules.OLEDController import OLEDController
 
 if __name__ == "__main__":
+    OLEDController().start_display_loop()
     LightController = LightController()
     LightController.run()
     
-    OLEDController().start_display_loop()
