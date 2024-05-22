af1d4c76d32a25a8dc9e66ef54c4b10c76564528
Mosunov Valeriy <zippwickness@gmail.com>
2024-05-22 23:29:16 +0300

    test

--- index.py
+++ index.py
@@ -1,8 +1,25 @@
+import threading
+import time
 from modules.LightController import LightController
 from modules.OLEDController import OLEDController
 
+
+def run_oled_controller():
+    oled_controller = OLEDController()
+    oled_controller.start_display_loop()
+
+
+def run_light_controller():
+    light_controller = LightController()
+    light_controller.run()
+
+
 if __name__ == "__main__":
-    OLEDController().start_display_loop()
-    LightController = LightController()
-    LightController.run()
-    
+    oled_thread = threading.Thread(target=run_oled_controller)
+    light_thread = threading.Thread(target=run_light_controller)
+
+    oled_thread.start()
+    light_thread.start()
+
+    oled_thread.join()
+    light_thread.join()
